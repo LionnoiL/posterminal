5e0da6f3bec629eb4ad29cfe7df127f6c3c0d209
Andriy Gaponov <wmconstructor@gmail.com>
2023-04-10 17:45:22 +0300

    first commit

---
+++ src/main/java/ua/gaponov/posterminal/Posterminal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ua.gaponov.posterminal;
+
+import ua.gaponov.posterminal.login.LoginForm;
+import ua.gaponov.posterminal.mainform.MainForm;
+
+/**
+ *
+ * @author wmcon
+ */
+public class Posterminal {
+
+    public static void main(String[] args) {
+        LoginForm.main(null);
+    }
+}
