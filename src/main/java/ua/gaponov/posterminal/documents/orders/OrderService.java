368c3edae4c23830cae0e1cc4591d443ebf140fa
gaponov <wmconstructor@gmail.com>
2023-04-25 20:43:09 +0300

    start order save

--- src/main/java/ua/gaponov/posterminal/documents/orders/OrderService.java
+++ src/main/java/ua/gaponov/posterminal/documents/orders/OrderService.java
@@ -1,49 +1,79 @@
 package ua.gaponov.posterminal.documents.orders;
 
+import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
-
 import ua.gaponov.posterminal.database.SqlHelper;
 import ua.gaponov.posterminal.database.StatementParameters;
-import ua.gaponov.posterminal.organization.Organization;
 
 /**
- *
  * @author gaponov
  */
 public class OrderService {
-    
-    public static Order getByGuid(String guid){
+
+    private static final SqlHelper<Order> helper = new SqlHelper<>();
+
+    public static Order getByGuid(String guid) {
         StatementParameters<String> parameters = StatementParameters.buildParameters(guid);
-        return new SqlHelper<Order>().getOne("select * from orders where order_guid = ?",
-                parameters,
-                new OrderDatabaseMapper());
+        return helper.getOne("select * from orders where order_guid = ?",
+            parameters,
+            new OrderDatabaseMapper());
     }
 
     public static List<Order> getAll() {
-        return new SqlHelper<Order>().getAll("SELECT * FROM orders", new OrderDatabaseMapper());
+        return helper.getAll("SELECT * FROM orders", new OrderDatabaseMapper());
     }
 
     public static List<Order> getAllNoUpload() {
         StatementParameters<Boolean> parameters = StatementParameters.buildParameters(false);
-        return new SqlHelper<Order>().getAll("SELECT * FROM orders where upload = ?",
-                parameters,
-                new OrderDatabaseMapper());
+        return helper.getAll("SELECT * FROM orders where upload = ?",
+            parameters,
+            new OrderDatabaseMapper());
     }
-    
-    public static void deleteAll(){
+
+    public static void deleteAll() {
         SqlHelper.execSql("delete from orders");
     }
-    
-    public static void deleteUploaded(){
+
+    public static void deleteUploaded() {
         String sql = """
-            SET REFERENTIAL_INTEGRITY FALSE;
-            BEGIN TRANSACTION;
-            DELETE FROM orders_detail WHERE order_guid IN (SELECT order_guid FROM orders WHERE upload = true);
-            DELETE FROM orders WHERE upload = true;
-            COMMIT;
-            SET REFERENTIAL_INTEGRITY TRUE;
-        """;
+                SET REFERENTIAL_INTEGRITY FALSE;
+                BEGIN TRANSACTION;
+                DELETE FROM orders_detail WHERE order_guid IN (SELECT order_guid FROM orders WHERE upload = true);
+                DELETE FROM orders WHERE upload = true;
+                COMMIT;
+                SET REFERENTIAL_INTEGRITY TRUE;
+            """;
+        SqlHelper.execSql(sql);
+    }
+
+    public static void save(Order order) throws SQLException {
+        Order findOrder = getByGuid(order.getGuid());
+        if (findOrder == null) {
+            insert(order);
+        } else {
+            update(order);
+        }
+    }
+
+    private static void update(Order order) {
+        String sql = """
+                SET REFERENTIAL_INTEGRITY FALSE;
+                BEGIN TRANSACTION;
+                
+                COMMIT;
+                SET REFERENTIAL_INTEGRITY TRUE;
+            """;
+        SqlHelper.execSql(sql);
+    }
+
+    private static void insert(Order order) {
+        String sql = """
+                SET REFERENTIAL_INTEGRITY FALSE;
+                BEGIN TRANSACTION;
+                
+                COMMIT;
+                SET REFERENTIAL_INTEGRITY TRUE;
+            """;
         SqlHelper.execSql(sql);
     }
 }
