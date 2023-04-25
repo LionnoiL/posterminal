3f71592b797aeae011f0eb7e8b7d3c6b15efaa18
Andriy Gaponov <wmconstructor@gmail.com>
2023-04-25 15:15:47 +0300

    refactoring

--- src/main/java/ua/gaponov/posterminal/database/Database.java
+++ src/main/java/ua/gaponov/posterminal/database/Database.java
@@ -4,30 +4,31 @@
  */
 package ua.gaponov.posterminal.database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.flywaydb.core.Flyway;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Objects;
+
 /**
- *
  * @author gaponov
  */
 public class Database {
-    
+
     private static BasicDataSource dataSource = null;
 
     static {
         dataSource = new BasicDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:./pos");
+        dataSource.setUrl("jdbc:h2:./pos;TRACE_LEVEL_FILE=0");
         dataSource.setUsername("");
         dataSource.setPassword("");
 
         dataSource.setMinIdle(5);
         dataSource.setMaxIdle(10);
         dataSource.setMaxTotal(25);
+        dataSource.setMaxOpenPreparedStatements(100);
 
         migrate();
     }
@@ -41,14 +42,11 @@ public class Database {
     }
 
     public static Connection getConnection() {
-        Connection connection = null;
         try {
-            connection = dataSource.getConnection();
+            return dataSource.getConnection();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
-        return connection;
     }
 
     public static void migrate() {
--- src/main/java/ua/gaponov/posterminal/database/SqlHelper.java
+++ src/main/java/ua/gaponov/posterminal/database/SqlHelper.java
@@ -4,41 +4,43 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
 /**
- *
  * @author gaponov
  */
 public class SqlHelper<T> {
-    
-    public SqlHelper(){
-    }
-            
-    public static void executeQuery(String sql, Consumer<ResultSet> consumer) {
 
-        try (Connection connection = Database.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql);
-             ResultSet resultSet = statement.executeQuery()
-        ) {
-            consumer.accept(resultSet);
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+    public SqlHelper() {
+    }
+
+    public static void execSql(String sql) {
+        try (Connection connection = Database.getConnection();) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.executeUpdate();
+            statement.close();
+        } catch (SQLException e) {
+            //NOP
+        }
+
+    }
+
+    public void execSql(String sql, StatementParameters parameters) throws SQLException {
+        try (Connection connection = Database.getConnection();) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            parameters.fillStatement(statement);
+            statement.executeUpdate();
+            statement.close();
         }
     }
-    
-     public List<T> getAll(String sql, StatementParameters parametrs, Mapper mapper) {
 
+    public List<T> getAll(String sql, StatementParameters parameters, Mapper mapper) {
         List<T> result = new ArrayList<T>();
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
-        try (Connection connection = Database.getConnection()) {
-            statement = connection.prepareStatement(sql);
-            parametrs.fillStatement(statement);
-            resultSet = statement.executeQuery();
+        try (Connection connection = Database.getConnection();) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            parameters.fillStatement(statement);
+            ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 result.add((T) mapper.map(resultSet));
             }
@@ -50,54 +52,28 @@ public class SqlHelper<T> {
         return result;
 
     }
-    
-    public List<T> getAll(String sql, Mapper<T> mapper) {
 
+    public List<T> getAll(String sql, Mapper<T> mapper) {
         List<T> result = new ArrayList<T>();
-        ResultSet resultSet = null;
-        try (Connection connection = Database.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-            resultSet = statement.executeQuery();
+        try (Connection connection = Database.getConnection();) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 result.add(mapper.map(resultSet));
             }
             resultSet.close();
+            statement.close();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
         return result;
-
     }
-    
-    public T getOne(String sql, StatementParameters parametrs, Mapper<T> mapper) {
 
-        List<T> list = getAll(sql, parametrs, mapper);
+    public T getOne(String sql, StatementParameters parameters, Mapper<T> mapper) {
+        List<T> list = getAll(sql, parameters, mapper);
         if (list.size() == 0) {
             return null;
         }
         return list.get(0);
-    }
-    
-    public void execSql(String sql, StatementParameters parametrs) throws SQLException {
-        PreparedStatement statement = null;
-
-        try (Connection connection = Database.getConnection()) {
-            statement = connection.prepareStatement(sql);
-            parametrs.fillStatement(statement);
-            statement.executeUpdate();
-            statement.close();
-        }
-    }
-    
-    public static void execSql(String sql) {
-        PreparedStatement statement = null;
-
-        try (Connection connection = Database.getConnection()) {
-            statement = connection.prepareStatement(sql);
-            statement.executeUpdate();
-            statement.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
     }
 }
