2ec1caf2bd66fe5058ed3b5f4d4c08a2f8f85a41
Andriy Gaponov <wmconstructor@gmail.com>
2024-03-13 11:09:14 +0200

    fix get last order number

--- src/main/java/ua/gaponov/posterminal/entity/orders/NumbersService.java
+++ src/main/java/ua/gaponov/posterminal/entity/orders/NumbersService.java
@@ -2,12 +2,19 @@ package ua.gaponov.posterminal.entity.orders;
 
 import ua.gaponov.posterminal.utils.PropertiesUtils;
 
+import static ua.gaponov.posterminal.utils.Constants.ORDER_NUMBER_NAME;
+
 public class NumbersService {
 
     public static long loadNumber(String key) {
         String number = PropertiesUtils.getApplicationTempValue(key);
         if (number == null || number.isBlank()) {
-            return 1;
+            switch (key) {
+                case ORDER_NUMBER_NAME:
+                    return OrderService.getCount() + 1;
+                default:
+                    return 1;
+            }
         }
         return Long.valueOf(number) + 1;
     }
